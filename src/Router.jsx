197c1944782731dc3632586506c730b1487c2150
jangyootae <sjang1425@naver.com>
2024-11-24 00:33:41 +0900

    [FEAT] - 라인그래프
    임시데이터로 일/주/월 나타냄

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import Home from './pages/Home'
+//import Home from './pages/Home'
 import Layout from './components/common/layout'
+import Mypage from './pages/mypage/mypage'
+import Home from './pages/Home'
+import DetailPage from './pages/mypage/DetailPage'
 
 const Router = () => {
   return (
@@ -14,6 +17,14 @@ const Router = () => {
           <Route
             index
             element={<Home />}
+          />
+          <Route
+            path="/mypage"
+            element={<Mypage />}
+          />
+          <Route
+            path="/detail/:date"
+            element={<DetailPage />}
           />
         </Route>
       </Routes>
