a64a97fae01363eb312f34cc681261b3bf5b0f04
jangyootae <sjang1425@naver.com>
2024-11-27 21:13:02 +0900

    [FEAT] - userInfo API 연결 코드 작성

--- src/components/mypage/UserInfo.jsx
+++ src/components/mypage/UserInfo.jsx
@@ -1,42 +1,43 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
 const UserInfo = () => {
-    // 더미 데이터 (API 호출을 대신하는 더미 데이터)
-    const userinfo = {
-        email: 'khsr98@naver.com',
-        nickname: '김민태',
-        age: 26,
-        gender: '남성',
-        occupation: '소프트웨어 개발자'
+    const [userinfo, setUserInfo] = useState(null)
+
+    const fetchData = async () => {
+        try {
+            const token = localStorage.getItem('access_token') // 토큰 가져오기
+            if (!token) {
+                throw new Error('Token not found') // 토큰이 없을 경우 에러 처리
+            }
+
+            const response = await axios.get(
+                `${import.meta.env.VITE_API_BASE_URL}/api/users/profile`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}` // 토큰 포함
+                    }
+                }
+            )
+
+            setUserInfo(response.data)
+        } catch (error) {
+            console.log(error)
+        }
     }
 
-    // const fetchData = async () => {
-    //     try {
-    //         const token = localStorage.getItem('access_token') // 토큰 가져오기
-    //         const response = await axios.get(
-    //             `${import.meta.env.VITE_API_BASE_URL}/api/users/profile`,
-    //             {
-    //                 headers: {
-    //                     Authorization: `Bearer ${token}` // 토큰 포함
-    //                 }
-    //             }
-    //         )
-    //         setUserInfo(response.data)
-    //     } catch (error) {
-    //         console.log(error)
-    //     }
-    // }
-
-    // useEffect(() => {
-    //     fetchData()
-    // }, [])
+    useEffect(() => {
+        fetchData()
+    }, [])
 
     if (!userinfo) {
         return <div>로딩 중...</div>
     }
+
     return (
         <div className="max-w-md mx-auto p-6 bg-white text-black shadow-md rounded-md">
-            {/* 사용자 정보 */}
             <p>
-                <strong>이름:</strong> {userinfo.nickname}
+                <strong>이름:</strong> {userinfo.name}
             </p>
             <p>
                 <strong>나이:</strong> {userinfo.age}세
