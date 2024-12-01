3ea338e0dd3292556b01da6f8bc7acd47e866b6d
liupei8979 <khsr98@ajou.ac.kr>
2024-12-01 22:07:40 +0900

    Feat: 내 프로필 받아오기 적용

--- src/pages/mypage/Mypage.jsx
+++ src/pages/mypage/Mypage.jsx
@@ -7,13 +7,12 @@ const Mypage = () => {
             {/* 메인 컨테이너 */}
             <div className="max-w-4xl h-full mx-auto flex flex-col gap-8 py-8 px-4">
                 {/* 유저 정보 섹션 */}
-                <div className="bg-white shadow-md rounded-lg p-6">
-                    <h2 className="text-xl font-bold mb-4 text-gray-700">나의 정보</h2>
+                <div>
                     <UserInfo />
                 </div>
 
                 {/* 통계 시각화 섹션 */}
-                <div className="bg-white shadow-md rounded-lg p-6  max-h-[1200px]">
+                <div className="bg-white shadow-md rounded-lg p-6 max-h-[1200px]">
                     <h2 className="text-xl font-bold mb-4 text-gray-700">통계 시각화 그래프</h2>
                     <StatisticalGraph />
                 </div>
